a526720ed428b55dd0ac1113cc0bc2d91c2f310a
Jacob Ferrante <contact@jacobferrante.com>
2020-10-01 16:21:32 -0400

    updated readme, and cleaned up some code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # configmaker
 config maker using python, jinja2 and yaml.
+
+
+It uses the yaml files for the questions to be asked and the txt files to be used as jinja2 templates.
+
+So far, the folders you need to have is "configurations" and "templates"
+
+When you run the script, it will ask which file you'd like to build, it will search the configurations folder for your choices to use.
+You must have a matching template to your yaml file, such as setup.yaml and setup.txt.
--- main.py
+++ main.py
@@ -15,28 +15,26 @@ def write_template(template_type, dict_name, file):
     print("Your configuration file is complete!")  
 
 # List the files within the YAML directory for user choice ###
-for root, dirs, files in os.walk("yaml"):
+for root, dirs, files in os.walk("configurations"):
     for filename in files:
         print(os.path.splitext(filename)[0])
-
+        
 ## Let the user choose the file to build and append .yaml to choice ###
+
 file_choice = input("What would you like to build a config for today? ")
 
-## load in the user choice file for the YAML and jinja2 template ###
-txt_files = os.path.join(os.path.dirname(os.path.abspath(__file__)), "/templates", file_choice )
-yaml_files = os.path.join(os.path.dirname(os.path.abspath(__file__)), "/yaml", file_choice)
 
-questions = glob.glob(yaml_files)
-templates = glob.glob(txt_files)
+## load in the user choice file for the YAML ###
+yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "configurations", file_choice + ".yaml")
 
-with open("yaml/questions.yaml") as file:
-    data = yaml.load(file, Loader=yaml.FullLoader)
+## open the user picked yaml file and load it
+with open(yaml_file) as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
 
+# assign new values to the dictionary based on user input
 for k, v in data.items():
     v["answer"] = input(v["question"])
-    #  you may or may not need this next line
     data[k] = v
 
-print(data)
-
-write_template(file_choice + ".txt", data, "output.ini")
+## write the output to a file
+write_template(file_choice + ".txt", data, file_choice)
