c6ec6143179e29471a7218ba5126b2332a037a6e
Jacob Ferrante <contact@jacobferrante.com>
2020-10-02 13:26:51 -0400

    cleaned up code

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ env = Environment (
 )
 
 ## Get the Jinja template and write to an output file
-def write_template(template_type, dict_name, file):
+def write_template(template_type, dict_name, template_file):
     template = env.get_template(template_type)
     output = template.render(**dict_name)
-    with open (file, 'w') as f:
+    with open (template_file, 'w') as f:
         f.write(output)
     print("Your configuration file is complete!")  
 
@@ -23,9 +23,9 @@ for root, dirs, files in os.walk("configurations"):
 
 file_choice = input("What would you like to build a config for today? ")
 
-
 ## load in the user choice file for the YAML ###
 yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "configurations", file_choice + ".yaml")
+output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "outputs")
 
 ## open the user picked yaml file and load it
 with open(yaml_file) as f:
