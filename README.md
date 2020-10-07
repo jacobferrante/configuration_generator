c05e8f89882b2bd1d21b69e7625467fcbeb2e771
Jacob Ferrante <contact@jacobferrante.com>
2020-10-07 00:40:45 -0400

    added precompiled functions, user choice, changed structure

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-# configmaker
-config maker using python, jinja2 and yaml.
+Configuration builder using Python, YAML, and Jinja2.
 
-
-It uses the yaml files for the questions to be asked and the txt files to be used as jinja2 templates.
-
-So far, the folders you need to have is "configurations" and "templates"
-
-When you run the script, it will ask which file you'd like to build, it will search the configurations folder for your choices to use.
-You must have a matching template to your yaml file, such as setup.yaml and setup.txt.
+It can be used by manually entering data or using precompiled YAML files with the data needed.
--- main.py
+++ main.py
@@ -7,43 +7,52 @@ env = Environment (
 )
 
 ## Get the Jinja template and write to an output file
-def write_template(template_type, dict_name, template_file):
+def write_template(dict_name, template_file, template_type):
     template = env.get_template(template_type)
     output = template.render(**dict_name)
     with open (template_file, 'w') as f:
         f.write(output)
     print("Your configuration file is complete!")  
+    
+## Ask the user if they're manually inputting data and print the file list based off choice
 
-# List the files within the YAML directory for user choice ###
-for root, dirs, files in os.walk("configurations"):
-    for filename in files:
-        print(os.path.splitext(filename)[0])
-        
-## Let the user choose the file to build and append .yaml to choice ###
+config_type = input("Are you manually filling out data? ")
 
-file_choice = input("What would you like to build a config for today? ")
-
-## load in the user choice file for the YAML ###
-yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "configurations", file_choice + ".yaml")
-output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "outputs")
+if config_type == "yes" "y":
+    for root, dirs, files in os.walk("configurations"):
+        for filename in files:
+            print(os.path.splitext(filename)[0])
+    file_choice = input("What data would you like to use? ")
+    yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "configurations", file_choice + ".yaml")    
+else:
+    for root, dirs, files in os.walk("precompiled"):
+        for filename in files:
+            print(os.path.splitext(filename)[0])
+    file_choice = input("What data would you like to use? ")
+    yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "precompiled", file_choice + ".yaml")
 
 ## open the user picked yaml file and load it
 with open(yaml_file) as f:
     data = yaml.load(f, Loader=yaml.FullLoader)
 
-
-# assign new values to the dictionary based on user input
-
-file_creation = input("Are you manually entering data? ")
-
-if file_creation == "yes":
+## If user chooses manual input, it will ask for input to reassign dictionary values
+if config_type == "yes":
     for k, v in data.items():
         v["answer"] = input(v["question"])
         data[k] = v
+## Otherwise it will use the reference file and reassign the values
 else:
     for k, v in data.items():
          data[k] = v
 
-
 ## write the output to a file
-write_template(file_choice + ".txt", data, file_choice)
+template_choice = input("what templates would you like to use? ")
+template_final = template_choice.split()
+
+for x in template_final:
+    write_template(data, x, x + ".txt")
+
+
+
+
+
