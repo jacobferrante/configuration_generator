bbde5428ccaaa2b7f432f526a30b60f0ce1ffaa9
Jacob Ferrante <contact@jacobferrante.com>
2020-10-12 21:21:53 -0400

    added some error checking

--- main.py
+++ main.py
@@ -12,24 +12,33 @@ def write_template(dict_name, template_file, template_type):
     output = template.render(**dict_name)
     with open (template_file, 'w') as f:
         f.write(output)
-    print("Your configuration file is complete!")  
+    print("Your configuration file is complete!")
+## Print off the file list in a directory
+def get_dir_list(folder):
+    for root, dirs, files in os.walk(folder):
+        for filename in files:
+            print(os.path.splitext(filename)[0])
     
 ## Ask the user if they're manually inputting data and print the file list based off choice
 
-config_type = input("Are you manually filling out data? ")
 
+config_type = None
+while config_type not in("yes", "no"):
+    config_type = input("Are you manually filling out data? (Yes, or No) ")
+file_choice = None
 if config_type == "yes":
-    for root, dirs, files in os.walk("configurations"):
-        for filename in files:
-            print(os.path.splitext(filename)[0])
-    file_choice = input("What data would you like to use? ")
-    yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "configurations", file_choice + ".yaml")    
-else:
-    for root, dirs, files in os.walk("precompiled"):
-        for filename in files:
-            print(os.path.splitext(filename)[0])
-    file_choice = input("What data would you like to use? ")
-    yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "precompiled", file_choice + ".yaml")
+    while file_choice not in("saewr", "asfsd"):
+        get_dir_list("configurations")
+        file_choice = input("What data would you like to use? ")
+        yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "configurations", file_choice + ".yaml")
+elif config_type == "no":
+    while file_choice not in():
+        get_dir_list("precompiled")
+        file_choice = input("What data would you like to use? ")
+        yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "precompiled", file_choice + ".yaml") 
+
+
+
 
 ## open the user picked yaml file and load it
 with open(yaml_file) as f:
@@ -49,9 +58,10 @@ else:
 template_choice = input("what templates would you like to use? ")
 template_final = template_choice.split()
 
-for x in template_final:
-    write_template(data, x, x + ".txt")
+file_ext = input("What is the file extension of your config? ")
 
+for x in template_final:
+    write_template(data, x + "." + file_ext, x + ".txt")
 
 
 
