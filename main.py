81420b754c852d23f269fab3fa125e9166fc4827
Jacob Ferrante <contact@jacobferrante.com>
2022-06-07 15:59:52 -0400

    make app useable
    now you can export files, and things are working

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import yaml
 import os
 import glob
 
+
+
+## Write dictionary to template/file
 def write_template(dict_name, template_file, template_type):
     template = env.get_template(template_type)
     output = template.render(**dict_name)
@@ -9,6 +12,7 @@ def write_template(dict_name, template_file, template_type):
         f.write(output)
     print("Your configuration file is complete!")
 
+## Function to generate a printed list of files in a directory
 def get_dir_list(file_ext):
     ret = set()
     for root, dirs, files in os.walk('questions'):
@@ -18,36 +22,59 @@ def get_dir_list(file_ext):
                 print(filename)
     return ret
 
-## Print out file list in template directory, only ending in .yaml and ask user which file, ask again if file does not exist
-file_choice = None
-files = get_dir_list(".yaml")
-yaml_dir = "questions"
+if __name__ == "__main__":
+    from jinja2 import FileSystemLoader, Environment
+    env = Environment (
+        loader = FileSystemLoader('questions/templates')
+    )
+    ## Print out file list in template directory, only ending in .yaml and ask user which file, ask again if file does not exist
+    file_choice = None
+    files = get_dir_list(".yaml")
+    yaml_dir = "questions"
+    
+    ## Print out list of templates, and have user choose a template and assign it to a variable to use
+    while file_choice not in files:
+        file_choice = input("What data would you like to use? ")
+        yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), yaml_dir, file_choice)
 
-while file_choice not in files:
-    file_choice = input("What data would you like to use? ")
-    yaml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), yaml_dir, file_choice)
+    ## Load YAML file into data dict
+    with open(yaml_file) as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
 
+    ## Save dicts. to variables to be used
+    prereqs = data["prereqs"]
+    questions = data ["questions"]
 
-## Load YAML file into data dict
-with open(template_choice) as f:
-    data = yaml.load(f, Loader=yaml.FullLoader)
+    ## Output filename based on user input or YAML file.
+    if prereqs["ask_filename"] == False:
+        print("using default filename")
+    else:
+        output_filename = input("What Filename would you like to use? ")
 
-## Save dicts. to variables to be used
-prereqs = data["prereqs"]
-questions = data ["questions"]
+    ## Output folder based on user input or YAML file.s
+    if prereqs["output_folder"] == None:
+        output_folder = input("Where would you like to output this? ")
+    else:
+        print("outputting file to " + prereqs["output_folder"])
+        output_folder = prereqs["output_folder"]
 
-## Output filename based on user input or YAML file.
-if prereqs["ask_filename"] == False:
-    print("using default filename")
-else:
-    output_filename = input("What Filename would you like to use? ")
+    ## assign values to dict item using user input 
+    for k, v in questions.items():
+        questions[k] = input(v)
 
-## Output folder based on user input or YAML file.s
-if prereqs["output_folder"] == None:
-    output_folder = input("Where would you like to output this? ")
-else:
-    print("outputting file to " + prereqs["output_folder"])
+    ## Check if output directory exists and create it if it doesn't
+    if os.path.isdir(output_folder) == False:
+        os.mkdir("outputs/" + output_folder)
+    else:
+        print("folder exists")
 
-## assign values to dict item using user input 
-for k, v in questions.items():
-    questions[k] = input(v)
+    
+    # Ask which template to use 
+    template_choice = input("what templates would you like to use? ")
+    template_final = template_choice.split()
+    
+
+    ## 
+    for x in template_final:
+        outputs = os.path.join(os.path.dirname(os.path.abspath(__file__)), output_folder, x)
+        write_template(questions, outputs, x)
